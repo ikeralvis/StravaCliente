9ef8424b87c821ecce2c7a914753bf4d37c2e35c
Iker <iker.a@opendeusto.es>
2025-01-17 16:45:09 +0100

    Refactor StravaRestTemplateServiceProxy; improve code readability by formatting URL construction and enhancing error handling

--- src/main/java/es/deusto/sd/strava/client/proxies/StravaRestTemplateServiceProxy.java
+++ src/main/java/es/deusto/sd/strava/client/proxies/StravaRestTemplateServiceProxy.java
@@ -18,8 +18,6 @@ import org.springframework.web.client.RestTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
 @Service
 public class StravaRestTemplateServiceProxy implements IStravaServiceProxy {
     private final RestTemplate restTemplate;
@@ -38,16 +36,17 @@ public class StravaRestTemplateServiceProxy implements IStravaServiceProxy {
         String peso = decimalFormat.format(usuario.peso());
         String altura = decimalFormat.format(usuario.altura());
 
-        String url = String.format("%s/auth/registroUsuario?email=%s&tipoLogin=%s&nombre=%s&fechaNacimiento=%s&peso=%s&altura=%s&frecuenciaCardiacaMax=%d&frecuenciaCardiacaReposo=%d",
-                               apiBaseUrl, 
-                                usuario.email(),
-                                usuario.tipoLogin(),
-                                usuario.nombre(), 
-                                usuario.fechaNacimiento(), 
-                                peso, 
-                                altura, 
-                                usuario.frecuenciaCardiacaMax(), 
-                                usuario.frecuenciaCardiacaReposo());
+        String url = String.format(
+                "%s/auth/registroUsuario?email=%s&tipoLogin=%s&nombre=%s&fechaNacimiento=%s&peso=%s&altura=%s&frecuenciaCardiacaMax=%d&frecuenciaCardiacaReposo=%d",
+                apiBaseUrl,
+                usuario.email(),
+                usuario.tipoLogin(),
+                usuario.nombre(),
+                usuario.fechaNacimiento(),
+                peso,
+                altura,
+                usuario.frecuenciaCardiacaMax(),
+                usuario.frecuenciaCardiacaReposo());
         logger.info("-RestTemplate- URL: " + url);
         try {
             return restTemplate.postForObject(url, null, String.class);
@@ -58,7 +57,8 @@ public class StravaRestTemplateServiceProxy implements IStravaServiceProxy {
                     throw new RuntimeException("Usuario ya registrado: " + e.getStatusCode().value());
                 case 401:
                     logger.info("-RestTemplate-    La contrasenya o el correo no son correctos");
-                    throw new RuntimeException("La contrasenya o el correo no son correctos: " + e.getStatusCode().value());
+                    throw new RuntimeException(
+                            "La contrasenya o el correo no son correctos: " + e.getStatusCode().value());
                 default:
                     throw new RuntimeException("Registro fallido: " + e.getStatusCode().value());
             }
@@ -67,8 +67,8 @@ public class StravaRestTemplateServiceProxy implements IStravaServiceProxy {
 
     @Override
     public void logout(String token) {
-        String url = apiBaseUrl + "/auth/logout"; 
-        
+        String url = apiBaseUrl + "/auth/logout";
+
         try {
             restTemplate.postForObject(url, token, Void.class);
         } catch (HttpStatusCodeException e) {
@@ -100,36 +100,36 @@ public class StravaRestTemplateServiceProxy implements IStravaServiceProxy {
                 default:
                     logger.error("-RestTemplate-    Login fallido: " + e.getStatusCode().value());
                     throw new RuntimeException("Login fallido: " + e.getStatusCode().value());
-            }}}
-
-            
-
-            @Override
-            public List<Entrenamiento> consultarEntrenamientos(String token, LocalDate fechaInicio, LocalDate fechaFin) {
-                // Construir la URL con los parámetros
-                String url = String.format("%s/api/entrenamientos/?token=%s%s%s",
-                        apiBaseUrl,
-                        token,
-                        fechaInicio != null ? "&fechaInicio=" + fechaInicio : "",
-                        fechaFin != null ? "&fechaFin=" + fechaFin : "");
-                logger.info("-RestTemplate- URL: " + url);
-                try {
-                    logger.info("-RestTemplate-    Procesando consulta de entrenamientos");
-                    return restTemplate.getForObject(url, List.class);
-                } catch (HttpStatusCodeException e) {
-                    switch (e.getStatusCode().value()) {
-                        case 401:
-                            logger.error("-RestTemplate-    Token inválido");
-                            throw new RuntimeException("Token inválido");
-                        default:
-                            logger.error("-RestTemplate-    Consulta de entrenamientos fallida: " + e.getStatusCode().value());
-                            throw new RuntimeException("Consulta de entrenamientos fallida: " + e.getStatusCode().value());
-                    }
-                }
             }
+        }
+    }
 
-            @Override
-            public void anadirEntrenamiento(String token, Entrenamiento entrenamiento) {
+    @Override
+    public List<Entrenamiento> consultarEntrenamientos(String token, LocalDate fechaInicio, LocalDate fechaFin) {
+        // Construir la URL con los parámetros
+        String url = String.format("%s/api/entrenamientos/?token=%s%s%s",
+                apiBaseUrl,
+                token,
+                fechaInicio != null ? "&fechaInicio=" + fechaInicio : "",
+                fechaFin != null ? "&fechaFin=" + fechaFin : "");
+        logger.info("-RestTemplate- URL: " + url);
+        try {
+            logger.info("-RestTemplate-    Procesando consulta de entrenamientos");
+            return restTemplate.getForObject(url, List.class);
+        } catch (HttpStatusCodeException e) {
+            switch (e.getStatusCode().value()) {
+                case 401:
+                    logger.error("-RestTemplate-    Token inválido");
+                    throw new RuntimeException("Token inválido");
+                default:
+                    logger.error("-RestTemplate-    Consulta de entrenamientos fallida: " + e.getStatusCode().value());
+                    throw new RuntimeException("Consulta de entrenamientos fallida: " + e.getStatusCode().value());
+            }
+        }
+    }
+
+    @Override
+    public void anadirEntrenamiento(String token, Entrenamiento entrenamiento) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'anadirEntrenamiento'");
     }
